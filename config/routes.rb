7643dd3437f1b08e0f47ccb22da29e347007c258
FrenchBully <kvng88@gmail.com>
2015-04-10 17:21:17 -0700

    styled just for splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'sessions#index'
-  resources :users
-  resources :brackets
+  root 'splash#index'
+  get '*path' => redirect('/')
+  # root 'sessions#index'
+  # resources :users
+  # resources :brackets
 
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/game_on', to: 'brackets#game_on', as: :game_on
-  match '/start_game', to: 'brackets#start_game', as: :start_game, via: [:get, :post]
-  post '/save_score', to: 'games#save_score'
+  # get '/auth/:provider/callback', to: 'sessions#create'
+  # get '/game_on', to: 'brackets#game_on', as: :game_on
+  # match '/start_game', to: 'brackets#start_game', as: :start_game, via: [:get, :post]
+  # post '/save_score', to: 'games#save_score'
 
 end
